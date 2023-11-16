45bc7eb77a361b004deb0ffc6e162cb5fd1a6998
Jacks Win10 <jackson.calixto@hotmail.com>
2023-11-16 04:55:35 -0300

    Creating Components with the CLI & Nesting Components

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule, FormsModule], // Add FormsModule to the imports array
   bootstrap: [AppComponent],
 })
