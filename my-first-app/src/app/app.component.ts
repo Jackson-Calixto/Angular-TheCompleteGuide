a3a53f47f831d1f4bc36fa8a0924b06f4d9a7b6c
Jacks Win10 <jackson.calixto@hotmail.com>
2023-12-04 04:34:02 -0300

    Working with Component Styles

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  //styleUrls: ['./app.component.css'], //it's between [] because is an array an can have multiple styles separated by ,
+  styles: [`
+    h3{
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {}
