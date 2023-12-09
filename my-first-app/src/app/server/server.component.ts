f04ad137c6a1a33d70d86ab444d228adb9d05117
Jacks Win10 <jackson.calixto@hotmail.com>
2023-12-09 02:02:04 -0300

    String Interpolation

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -1,10 +1,14 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
 })
 export class ServerComponent {
-  serverID:number = 10;
-  serverStatus:string = 'offline';
+  serverID: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
