4f8d020c044bf0ad666bbbe30a68f080b5b4e230
Jacks Win10 <jackson.calixto@hotmail.com>
2023-12-09 01:38:24 -0300

    Assignment 1: Practicing Components

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent],
   imports: [BrowserModule, FormsModule], // Add FormsModule to the imports array
   bootstrap: [AppComponent],
 })
