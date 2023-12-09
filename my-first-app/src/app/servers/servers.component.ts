3b708b54321321a76c78340c94e2271cf36ed0f5
Jacks Win10 <jackson.calixto@hotmail.com>
2023-12-09 02:13:32 -0300

    Property Binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers', //by element
   //selector: '[app-servers]', //by attribut
   //selector: '.app-servers', //by class
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
-  styleUrls: ['./servers.component.css']
+  templateUrl: './servers.component.html',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // `,
+  styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  
+  constructor() {}
 
+  ngOnInit() {}
 }
