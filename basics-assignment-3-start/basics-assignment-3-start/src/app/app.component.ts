1a32620ac62dd3f4a0955e0177982c9c90373b0c
Jacks Win10 <jackson.calixto@hotmail.com>
2024-01-16 10:57:43 -0300

    basics-assignment-3-finishing

--- basics-assignment-3-start/basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { timestamp } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  displayDetails: boolean;
+  timestamps = [];
+
+  setDisplayDetails() {
+    this.displayDetails = !this.displayDetails;
+    this.timestamps.push(new Date())
+  }
 }
