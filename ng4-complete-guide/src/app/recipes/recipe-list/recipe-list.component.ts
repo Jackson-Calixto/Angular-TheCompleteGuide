cee9a9123ce0770c39b615e4f75848c3a598244e
Jacks Win10 <jackson.calixto@hotmail.com>
2024-02-22 08:31:01 -0300

    Adding Content to the Recipes Components

--- ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://easyweeknightrecipes.com/wp-content/uploads/2021/06/meatloaf-recipe-4.jpg')
+  ];
 
   constructor() {}
 
