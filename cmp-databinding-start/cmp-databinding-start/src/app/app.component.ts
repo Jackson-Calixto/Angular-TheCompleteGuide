5da9afabf003683e8d71b7a28daade46b77a7904
Jacks Win10 <jackson.calixto@hotmail.com>
2024-03-18 06:58:25 -0300

    Seeing Lifecycle Hooks in Action

--- cmp-databinding-start/cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/cmp-databinding-start/src/app/app.component.ts
@@ -3,24 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+  serverElements = [
+    { type: 'server', name: 'Testserver', content: 'Just a test!' },
+  ];
 
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
     this.serverElements.push({
       type: 'server',
       name: serverData.serverName,
-      content: serverData.serverContent
+      content: serverData.serverContent,
     });
   }
 
-  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
     this.serverElements.push({
       type: 'blueprint',
       name: blueprintData.serverName,
-      content: blueprintData.serverContent
+      content: blueprintData.serverContent,
     });
   }
 }
--- cmp-databinding-start/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,11 +7,49 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit{
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
   constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    console.log('ngOnChanges called!');
+    console.log(changes);
   }
 
   ngOnInit() {
+    console.log('ngOnInit called!');
   }
 
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    console.log('ngDoCheck called!')
+  }
+
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked(): void {
+    //Called after every check of the component's or directive's content.
+    //Add 'implements AfterContentChecked' to the class.
+    console.log('ngAfterContentChecked called!')
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called!')
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called!');
+  }
 }
